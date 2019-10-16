906a0cfa7d9039ad76eacc8bb9942a1625a6f2c9
Ryo-Bamboo <bamboo.developer@gmail.com>
2019-10-17 00:41:46 +0900

    Implement 4-6

--- ch04-Trees-and-Graphs/4-6.py
+++ ch04-Trees-and-Graphs/4-6.py
@@ -2,3 +2,23 @@
 Problem:
 Solution:
 """
+
+
+def get_left_most_progeny(node):
+    while node is not None:
+        if node.left is None:
+            return node
+        node = node.left
+
+    return None
+
+
+def get_successor(node):
+    successor = get_left_most_progeny(node.right_node)
+    if successor is None:
+        parent = node.parent
+        while parent is None or parent.left != node:
+            parent = node.parent
+        successor = parent
+
+    return successor
