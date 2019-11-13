3728c746e1d75bd688d17fb685decc9fa48efff6
Ryo-Bamboo <bamboo.developer@gmail.com>
2019-11-13 23:27:48 +0900

    Implementing 8-2

---
+++ ch08-Recursive-Dynamic-Programing/8-2.py
@@ -0,0 +1,21 @@
+import queue
+
+
+def getPath(maze, y, x, checked_points):
+    if checked[y][x] = True
+    q = queue.Queue()
+
+    while q.not_empty:
+        if current_point in checked_points:
+            return False
+
+
+maze = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+print(getPath(maze, 0, 0, None))
