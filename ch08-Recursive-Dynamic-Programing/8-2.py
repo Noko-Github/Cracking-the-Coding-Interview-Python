83057c3f70044dcb9cacb27dc77be490fd76e680
Ryo-Bamboo <bamboo.developer@gmail.com>
2019-11-14 23:53:36 +0900

    Implement 8-2

--- ch08-Recursive-Dynamic-Programing/8-2.py
+++ ch08-Recursive-Dynamic-Programing/8-2.py
@@ -1,21 +1,34 @@
 import queue
 
 
-def getPath(maze, y, x, checked_points):
-    if checked[y][x] = True
-    q = queue.Queue()
+def robot_grid(maze):
+    path = []
+    point = (len(maze)-1, len(maze[0])-1)
+    if get_path(maze, point, path):
+        return path
 
-    while q.not_empty:
-        if current_point in checked_points:
-            return False
+    return None
+
+
+def get_path(maze, point, path):
+    if point[0] < 0 or point[1] < 0 or maze[point[0]][point[1]]:
+        return False
+
+    is_origin = point == (0, 0)
+
+    if is_origin or get_path(maze, (point[0] - 1, point[1]), path) or get_path(maze, (point[0], point[1] - 1), path):
+        path.append(point)
+        return True
+
+    return False
 
 
 maze = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
+    [0, 1, 1, 1, 1],
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0]
 ]
 
-print(getPath(maze, 0, 0, None))
+print(robot_grid(maze))
