78f31d56a4d052134cbcc036d54626ddc5bbd0d9
Ryo-Bamboo <bamboo.developer@gmail.com>
2019-11-21 23:36:24 +0900

    Implement 5-4

--- ch05-Bit-Manipulation/5-4.py
+++ ch05-Bit-Manipulation/5-4.py
@@ -10,13 +10,34 @@ def get_next(num):
         c1 += 1
         bit >>= 1
 
-    if c0+c1 == len(num) or c0+c1 == 0:
+    if c0+c1 == num or c0+c1 == 0:
         return None
+
+    num |= 1 << c0+c1
+    num &= ~((1 << c0+c1) - 1)
+    num |= (1 << (c1-1)) - 1
+    return num
 
 
 def get_prev(num):
-    pass
+    bit = num
+    c0 = 0
+    c1 = 0
+    while (bit & 1 == 0) and (bit != 0):
+        c0 += 1
+        bit >>= 1
+
+    while (bit & 1) == 1:
+        c1 += 1
+        bit >>= 1
+
+    if c0+c1 == num or c0+c1 == 0:
+        return None
+
+    num &= ~((1 << c0+c1) - 1)
+    num |= ((1 << c1) - 1) << (c0-1)
 
 
 num = 13948
 print(get_next(num))
+print(get_prev(num))
