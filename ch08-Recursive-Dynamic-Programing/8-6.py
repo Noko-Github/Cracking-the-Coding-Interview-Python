23e8778bb8672470af609d035e78bd674bb3ed74
Ryo-Bamboo <bamboo.developer@gmail.com>
2019-12-02 23:53:10 +0900

    Implementing 8-6

--- ch08-Recursive-Dynamic-Programing/8-6.py
+++ ch08-Recursive-Dynamic-Programing/8-6.py
@@ -1,3 +1,31 @@
+from collections import deque
+
+
 class Tower:
-    def move_disk(self, n, destination, buffer):
-        pass
+
+    def __init__(self, i):
+        self.disks = deque([])
+        self.index = i
+
+    def add(self, n):
+        self.disks.append(n)
+
+    def move_disks(self, n, origin, destination, buffer):
+        if n <= 0:
+            return None
+
+        self.move_disks(n-1, origin, buffer, destination)
+
+        self.move_top_to(self, destination)
+
+        self.move_disks(n-1, buffer, destination, origin)
+
+    def move_top_to(self, destination):
+        top = self.disks.pop()
+        self.destination.add(top)
+
+
+n = 3
+towers = [Tower(i) for i in range(3)]
+for i in n:
+    towers[0].add(i)
