b4c26d31e4b0018c7de1ac3768d70af692bf6696
Ryo-Bamboo <bamboo.developer@gmail.com>
2019-12-07 15:11:21 +0900

    implement 10-8

---
+++ ch10-Sort-and-Search/10-8.py
@@ -0,0 +1,32 @@
+class BitSet:
+
+    def __init__(self, size):
+        self.bitset = [0] * ((size >> 5)+1)
+
+    def get(self, pos):
+        word_number = (pos >> 5)
+        bit_number = (pos & 0x1f)
+        return (self.bitset[word_number] & (1 << bit_number)) != 0
+
+    def set(self, pos):
+        word_number = (pos >> 5)
+        bit_number = (pos & 0x1f)
+        self.bitset[word_number] |= 1 << bit_number
+
+
+def check_duplicates(array):
+    bit = BitSet(32000)
+    duplicate_values = []
+    for pos in range(len(array)):
+        number = array[pos]
+        num0 = number - 1
+        if bit.get(num0):
+            duplicate_values.append(number)
+        else:
+            bit.set(num0)
+
+    return duplicate_values
+
+
+array = [1, 2, 3, 4, 4, 5, 5, 6, 7, 8, 8, 9]
+print(check_duplicates(array))
