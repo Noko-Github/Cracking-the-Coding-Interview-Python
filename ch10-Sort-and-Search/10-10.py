8a8c9d21942d3d8e210569d016446087c183205b
Ryo-Bamboo <bamboo.developer@gmail.com>
2019-12-14 23:19:56 +0900

    Implement 10-10

--- ch10-Sort-and-Search/10-10.py
+++ ch10-Sort-and-Search/10-10.py
@@ -1,5 +1,41 @@
-def get_rank(node, x):
-    if node.value == x:
-        return node.left_size()
-    if node.value > x:
-        return get_rank(node.left, x)
+def track(root, number):
+    if root == None:
+        root = RankNode(number)
+    else:
+        root.insert(number)
+
+
+class RankNode():
+    def __init__(self, data):
+        self.left_size = 0
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, d):
+        if d <= self.data:
+            if self.left is not None:
+                self.left.insert(d)
+            else:
+                self.left = RankNode(d)
+            self.left_size += 1
+        else:
+            if self.right is not None:
+                self.right.insert(d)
+            else:
+                self.right = RankNode(d)
+
+    def get_rank(self, d):
+        if self.data == d:
+            return self.left_size
+        elif d < self.data:
+            if self.left is None:
+                return None
+            else:
+                return self.left.get_rank(d)
+        else:
+            right_rank = -1 if self.right is None else self.right.get_rank(d)
+            if right_rank == -1:
+                return -1
+            else:
+                return self.left_size + 1 + right_rank
